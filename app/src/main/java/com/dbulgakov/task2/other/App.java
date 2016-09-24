33cb2698ebfd319ca486b29646e056d61c6ff2da
Dmitry Bulgakov <mail@dbulgakov.com>
2016-09-24 12:06:37 +0300

    added get context method

--- app/src/main/java/com/dbulgakov/task2/other/App.java
+++ app/src/main/java/com/dbulgakov/task2/other/App.java
@@ -1,6 +1,7 @@
 package com.dbulgakov.task2.other;
 
 import android.app.Application;
+import android.content.Context;
 import android.util.Log;
 
 import com.dbulgakov.task2.other.di.AppComponent;
@@ -9,14 +10,20 @@ import com.dbulgakov.task2.other.di.DaggerAppComponent;
 
 public class App extends Application {
     private static AppComponent component;
+    private static App appInstance;
 
     public static AppComponent getComponent() {
         return component;
     }
 
+    public static Context getContext() {
+        return appInstance.getApplicationContext();
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
+        appInstance = this;
         component = buildComponent();
     }
 
