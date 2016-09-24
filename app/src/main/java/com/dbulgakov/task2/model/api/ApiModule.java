7c341599b2b167e03cd35aa3fb52778f2251c495
Dmitry Bulgakov <mail@dbulgakov.com>
2016-09-24 12:21:32 +0300

    added cache for retrofit

--- app/src/main/java/com/dbulgakov/task2/model/api/ApiModule.java
+++ app/src/main/java/com/dbulgakov/task2/model/api/ApiModule.java
@@ -1,7 +1,12 @@
 package com.dbulgakov.task2.model.api;
 
 import com.dbulgakov.task2.model.api.ApiInterface;
+import com.dbulgakov.task2.other.App;
 
+import java.io.File;
+import java.util.UUID;
+
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -14,12 +19,16 @@ public final class ApiModule {
 
     public static ApiInterface getApiInterface(String url) {
 
+        File cacheDir = new File(App.getContext().getCacheDir(), UUID.randomUUID().toString());
+        Cache cache = new Cache(cacheDir, 1024);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().cache(cache).build();
+
+
         Retrofit.Builder builder = new Retrofit.Builder().
                 baseUrl(url)
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create());
-
-        builder.client(new OkHttpClient());
 
         return builder.build().create(ApiInterface.class);
     }
