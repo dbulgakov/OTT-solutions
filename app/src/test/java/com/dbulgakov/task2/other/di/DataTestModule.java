f81f2f6b63a02aa602daa4c55624c137af59d456
Dmitry Bulgakov <mail@dbulgakov.com>
2016-09-25 23:57:05 +0300

    DataTestModule update

--- app/src/test/java/com/dbulgakov/task2/other/di/DataTestModule.java
+++ app/src/test/java/com/dbulgakov/task2/other/di/DataTestModule.java
@@ -1,8 +1,15 @@
 package com.dbulgakov.task2.other.di;
 
+import com.dbulgakov.task2.model.pojo.UserOrder;
 import com.dbulgakov.task2.other.TestUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
 
 @Module
 public class DataTestModule {
@@ -10,5 +17,12 @@ public class DataTestModule {
 
     public DataTestModule() {
         testUtils = new TestUtils();
+    }
+
+    @Provides
+    @Singleton
+    List<UserOrder> provideUserOrderList() {
+        UserOrder[] userOrderArray = testUtils.getGson().fromJson(testUtils.readString("json/orders"), UserOrder[].class);
+        return Arrays.asList(userOrderArray);
     }
 }
