5675acca070255cd32fe68451c1dcbf4c31713f4
Dmitry Bulgakov <mail@dbulgakov.com>
2016-09-26 01:55:40 +0300

    bugfix, starting to implement ModelImplTest

--- app/src/test/java/com/dbulgakov/task2/model/ModelImplTest.java
+++ app/src/test/java/com/dbulgakov/task2/model/ModelImplTest.java
@@ -1,4 +1,22 @@
 package com.dbulgakov.task2.model;
 
-public class ModelImplTest {
+import com.dbulgakov.task2.model.api.ApiInterface;
+import com.dbulgakov.task2.other.BaseTest;
+
+import org.junit.Before;
+
+import javax.inject.Inject;
+
+public class ModelImplTest extends BaseTest{
+    @Inject
+    ApiInterface apiInterface;
+
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        component.inject(this);
+        model = new ModelImpl();
+    }
 }
--- app/src/test/java/com/dbulgakov/task2/other/di/TestComponent.java
+++ app/src/test/java/com/dbulgakov/task2/other/di/TestComponent.java
@@ -1,5 +1,7 @@
 package com.dbulgakov.task2.other.di;
 
+import com.dbulgakov.task2.model.ModelImplTest;
+import com.dbulgakov.task2.presenter.BasePresenter;
 import com.dbulgakov.task2.presenter.OrdersTestPresenter;
 import com.dbulgakov.task2.view.fragments.OrdersTestFragment;
 
@@ -10,9 +12,11 @@ import dagger.Component;
 @Singleton
 @Component(modules = {ModelTestModule.class, PresenterTestModule.class, ViewTestModule.class, DataTestModule.class})
 public interface TestComponent extends AppComponent {
-    void inject(ModelTestModule modelTestModule);
+    void inject(ModelImplTest modelImplTest);
 
     void inject(OrdersTestPresenter ordersTestPresenter);
+
+    void inject(BasePresenter basePresenter);
 
     void inject(OrdersTestFragment ordersTestFragment);
 }
