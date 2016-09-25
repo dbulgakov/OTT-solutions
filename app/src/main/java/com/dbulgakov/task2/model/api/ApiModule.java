e433628d2a05a5962b5bdeb9eb3c428e040c5b89
Dmitry Bulgakov <mail@dbulgakov.com>
2016-09-25 21:38:52 +0300

    minor cache updates, gradle build speed improvements

--- app/src/main/java/com/dbulgakov/task2/model/api/ApiModule.java
+++ app/src/main/java/com/dbulgakov/task2/model/api/ApiModule.java
@@ -5,6 +5,7 @@ import com.dbulgakov.task2.other.App;
 
 import java.io.File;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
@@ -13,6 +14,13 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public final class ApiModule {
+
+    private static final int CONNECT_TIMEOUT = 45;
+    private static final int WRITE_TIMEOUT = 45;
+    private static final int READ_TIMEOUT = 45;
+    private static final long CACHE_SIZE = 10 * 1024 * 1024;
+
+
     private ApiModule() {
 
     }
@@ -20,8 +28,12 @@ public final class ApiModule {
     public static ApiInterface getApiInterface() {
 
         File cacheDir = new File(App.getContext().getCacheDir(), "response");
-        Cache cache = new Cache(cacheDir, 4096);
-        OkHttpClient okHttpClient = new OkHttpClient.Builder().cache(cache).build();
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .cache(new Cache(cacheDir, CACHE_SIZE))
+                .connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(READ_TIMEOUT, TimeUnit.SECONDS)
+                .writeTimeout(WRITE_TIMEOUT, TimeUnit.SECONDS)
+                .build();
 
 
         Retrofit.Builder builder = new Retrofit.Builder().
