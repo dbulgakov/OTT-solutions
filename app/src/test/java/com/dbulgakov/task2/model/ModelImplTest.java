d570e9f1c2fa3aa3af4dad6e71af51ad777bba02
Dmitry Bulgakov <mail@dbulgakov.com>
2016-09-26 13:02:43 +0300

    added ModelImplTest, added files required for future tests

--- app/src/test/java/com/dbulgakov/task2/model/ModelImplTest.java
+++ app/src/test/java/com/dbulgakov/task2/model/ModelImplTest.java
@@ -1,11 +1,22 @@
 package com.dbulgakov.task2.model;
 
 import com.dbulgakov.task2.model.api.ApiInterface;
+import com.dbulgakov.task2.model.pojo.UserOrder;
 import com.dbulgakov.task2.other.BaseTest;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Inject;
+
+import rx.Observable;
+import rx.observers.TestSubscriber;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class ModelImplTest extends BaseTest{
     @Inject
@@ -18,5 +29,30 @@ public class ModelImplTest extends BaseTest{
         super.setUp();
         component.inject(this);
         model = new ModelImpl();
+    }
+
+    @Test
+    public void testGetUserOrders() {
+        UserOrder[] userOrders = testUtils.getGson().fromJson(testUtils.readString("json/orders"), UserOrder[].class);
+
+        when(apiInterface.getUserOrders()).thenReturn(Observable.just(Arrays.asList(userOrders)));
+
+        TestSubscriber<List<UserOrder>> testSubscriber = new TestSubscriber<>();
+        model.getUserOrders().subscribe(testSubscriber);
+
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertValueCount(1);
+
+        List<UserOrder> actual = testSubscriber.getOnNextEvents().get(0);
+
+        assertEquals(8, actual.size());
+        assertEquals("Vim airlines", actual.get(0).getAirline());
+        assertEquals("DME", actual.get(0).getOrigin());
+        assertEquals("LED", actual.get(0).getDestination());
+        assertEquals("NN-775", actual.get(0).getFlightNumber());
+        assertEquals(false, actual.get(0).getIfUserCancel());
+
+        assertEquals(0, actual.get(3).getStopNumber());
+        assertEquals("4:30", actual.get(3).getFlightDuration());
     }
 }
---
+++ app/src/test/java/com/dbulgakov/task2/predicate/ActiveOrderPredicateTest.java
@@ -0,0 +1,4 @@
+package com.dbulgakov.task2.predicate;
+
+public class ActiveOrderPredicateTest {
+}
